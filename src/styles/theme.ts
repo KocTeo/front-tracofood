7549feb9de6b9564c24a94d4c5873cae7d31d28b
Alexandre K. Teodoreto <alekteodoreto@hotmail.com>
2023-05-17 22:20:40 -0300

    build: Configurando styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+const theme = {
+  colors: {
+    primary: '#6A0000',
+    secondary: '#DC9000',
+    secondary_light: '#E6A11F',
+    util: '#223263',
+    disable: '##C8C8C8'
+  },
+
+  font: {
+    size: {
+      xsm: '1.2rem',
+      sm: '1.8rem',
+      md: '2rem',
+      lg: '2.2rem',
+      xlg: '2.4rem'
+    },
+
+    opacity: {
+      md: '50%'
+    },
+
+    weight: {
+      normal: 500,
+      bold: 700
+    }
+  },
+
+  radius: {
+    litle: '0.5rem',
+    full: '50%'
+  }
+};
+
+export default theme;
