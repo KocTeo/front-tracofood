7068cf202ed560fc3d501ae92004bcddf1bab97f
Alexandre K. Teodoreto <alekteodoreto@hotmail.com>
2023-05-20 14:40:34 -0300

    refactor: Redirecionando para a tela de Login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,13 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react"
+
 export default function Home() {
+  const route = useRouter();
+  
+  useEffect(() => {
+    route.push('/login')
+  });
+
   return (
     <>
       <h1>Agora vai</h1>
