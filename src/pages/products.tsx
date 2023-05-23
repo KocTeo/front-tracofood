d3df2bde5cf927cbe2432dacec991ffeae7463bb
Alexandre K. Teodoreto <alekteodoreto@hotmail.com>
2023-05-23 16:54:04 -0300

    feat: Renderiazando a página de Produtos

---
+++ src/pages/products.tsx
@@ -0,0 +1,11 @@
+import { ProductLayout } from "@/components/Layouts/ProductsLayout";
+import { Header } from "@/components/Molecules/Products/Header";
+import { NavBarContainer } from "@/components/Organisms/NavBarContainer";
+
+export default function Products() {
+  return (
+    <>
+      <ProductLayout />
+    </>
+  )
+}
